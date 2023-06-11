f1892e3593a5b974f9597bc2f3f847589d18ddae
Noah8863 <stacknoah99@gmail.com>
2023-06-11 12:55:49 -0600

    set up simple useState counter for practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './main.scss';
 
 function App() {
+  var [count,setCount] = useState(0);
+
+  function getCount() {
+    setCount(count + 1)
+  }
+  function decreaseCount() {
+    setCount(count - 1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getCount}>Click Me for Increase!</button>
+      <button onClick={decreaseCount}>Click Me for Decrease!</button>
+      <p id="count">Count is: {count}</p>
     </div>
   );
 }
