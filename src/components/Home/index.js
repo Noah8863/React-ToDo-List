401420a13078670ae17394ea9b20a7e5932199c4
Noah Hoffman <stacknoah99@gmail.com>
2023-06-12 12:06:24 -0600

    basic useState function before bed

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.scss";
 
 function Home(){
+    const input = document.getElementById("inputField");
+    const [value, newValue] = useState("");
+
+    function submitNewEvent(){
+        newValue(console.log(input.value));
+    }
     return (
         <div id="mainContainer">
             <header>
                 <h1 style={{padding: 20}}>ToDo List</h1>
-                <input type="text" placeholder="New Event Here..." />
-                <button >Add Event</button>
+                <input type="text" id="inputField" placeholder="New Event Here..." />
+                <button onClick={submitNewEvent}>Add Event</button>
             </header>
             <div className="newEventContainer">
-                Hello
+                This is where the new event would go: {value}
             </div>
         </div>
     )
